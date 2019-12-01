666b9d62352510483071572696eaae75855163f0
Aidiakapi <aidiakapi@gmail.com>
2019-12-01 13:56:11 +0100

    Open input after downloading with VS Code.

--- input_dl/src/main.rs
+++ input_dl/src/main.rs
@@ -1,4 +1,5 @@
 use reqwest::{Client, StatusCode};
+use std::process::Command;
 
 fn main() {
     let day_nr: u8 = match std::env::args()
@@ -23,31 +24,41 @@ fn main() {
     };
 
     let client = Client::new();
-    match client
+    let (resp, text) = match client
         .get(&url)
         .header("cookie", format!("session={}", token))
         .send()
         .and_then(|mut resp| resp.text().map(|text| (resp, text)))
     {
-        Ok((resp, text)) => {
-            if resp.status() != StatusCode::OK {
-                eprintln!("expected HTTP 200 OK status, got {:?}", resp.status());
-                std::process::exit(-1);
-            }
-
-            match std::fs::create_dir_all("./data/")
-                .and_then(|_| std::fs::write(format!("./data/day{:0>2}.txt", day_nr), text))
-            {
-                Ok(()) => {}
-                Err(err) => {
-                    eprintln!("couldn't write downloaded input to file:\n{:?}", err);
-                    std::process::exit(-1);
-                }
-            }
-        }
+        Ok(x) => x,
         Err(err) => {
             eprintln!("http error:\n{:?}", err);
             std::process::exit(-1);
         }
+    };
+    if resp.status() != StatusCode::OK {
+        eprintln!("expected HTTP 200 OK status, got {:?}", resp.status());
+        std::process::exit(-1);
+    }
+
+    let file_path = format!("./data/day{:0>2}.txt", day_nr);
+    match std::fs::create_dir_all("./data/").and_then(|_| std::fs::write(&file_path, text)) {
+        Ok(()) => {}
+        Err(err) => {
+            eprintln!("couldn't write downloaded input to file:\n{:?}", err);
+            std::process::exit(-1);
+        }
+    }
+
+    let _ = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .arg("/C")
+            .arg(&format!("code {}", file_path))
+            .spawn()
+    } else {
+        Command::new("sh")
+            .arg("-c")
+            .arg(&format!("code {}", file_path))
+            .spawn()
     };
 }
